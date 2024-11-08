18bdf969b78c3bdc87082a948969710bb711d3d1
Ilay Sh <ilayshlomo@gmail.com>
2024-11-08 13:39:48 +0200

    8.11 mac
    player control adjustment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int jumpForce = 10;
+    [SerializeField] int moveSpeed = 10;
+    Rigidbody2D rb;
     void Start()
     {
-        
+       rb = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+    //idea: what if it's a sliding platform and the player can hop small distances (go side but a bit up) and slide
     void Update()
     {
-        
+    if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+    Debug.Log("go left");
+ }
+    if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+    Debug.Log("go right");
+ }
+    if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)){
+    Debug.Log("jump");
+ }
+    if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
+    Debug.Log("go left");
+ }
     }
 }
